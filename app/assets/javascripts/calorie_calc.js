2830b9bcaca4c02721730bf817ac2db692455070
Nicholas Wargnier <nwargnier@gmail.com>
2013-09-09 17:04:46 -0700

    compelete calorie calculator

--- app/assets/javascripts/calorie_calc.js
+++ app/assets/javascripts/calorie_calc.js
@@ -3,12 +3,18 @@ var calorieCalcView = {
     var $inputs = $('form :input');
     var values = {};
     $inputs.each( function() {
-      values[this.name] = $(this).val();
+      if($(this).attr('type') === 'radio') {
+        if($(this).is(':checked')) {
+          values[this.name] = $(this).val();
+        }
+      } else {
+        values[this.name] = $(this).val();
+      }
     });
     return values;
   },
-  displayCalories: function() {
-    // display color after calculation to screen
+  displayCalories: function(calories) {
+    console.log(calories);
   }
 }
 
@@ -25,6 +31,40 @@ var calorieCalcController = {
   }
 }
 
+
+
 function calorieCalc(data) {
-  console.log(data);
+  data.gender === "male" ? maleCalc(data) : femaleCalc(data)
+}
+
+// Men: 66 + (6.23 x weight in pounds) + (12.7 x height in inches) - (6.8 x age in years)
+function maleCalc(data) {
+  var calories =  ( 66 + (6.23 * (data.weight)) + (12.7 * heightToInches(data.feet, data.inches)) - (6.8 * data.age) ) * data.activity + goal(data.goal);
+  calorieCalcView.displayCalories(calories);
+}
+
+// Women: 655 + (4.35 x weight in pounds) + (4.7 x height in inches) - (4.7 x age in years)
+function femaleCalc(data) {
+  var calories = ( 655 + (4.35 * data.weight)) + (4.7 * heightToInches(data.feet, data.inches) - (4.7 * data.age) ) * data.activity + goal(data.goal);
+  calorieCalcView.displayCalories(calories);
+}
+
+function heightToInches(feet, inches) {
+  return parseInt(feet) * 12 + parseInt(inches);
+}
+
+function goal(goal) {
+  var goalNum;
+  switch (goal) {
+    case "muscle":
+      goalNum = 500;
+      break;
+    case "maintenance":
+      goalNum = 0;
+      break;
+    case "weightloss":
+      goalNum = -500;
+      break;
+  }
+  return goalNum;
 }
